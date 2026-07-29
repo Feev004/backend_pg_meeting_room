d38d5a14ac5eb89521d3a4015dc69c090a71194e
Ratthasart Rojjanai <feevfeev2@gmail.com>
2026-07-29 12:01:56 +0700

    up

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require('express');
+const express = require("express");
+const DB = require("./DB");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Express.js server is running!');
+app.use(express.json());
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.send("Express.js server is running!");
+// });
+
+app.get("/api/test_", async (req, res) => {
+  try {
+    const sql =
+      `SELECT user.u_id, user.u_name, user.u_surname, user.password, role.role, status.status FROM user
+INNER JOIN role ON user.role = role.r_id
+INNER JOIN status ON user.status = status.s_id;`
+    const [products] = await DB.query(sql);
+    // console.log("test",products);
+    
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
