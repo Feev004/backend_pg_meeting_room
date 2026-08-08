2ee80f08b5bb4e232945de8eae937a628c67f963
Ratthasart Rojjanai <feevfeev2@gmail.com>
2026-08-08 15:15:12 +0700

    up

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
 const express = require("express");
 const DB = require("./DB");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: "http://192.168.201.3:5173", // Frontend
+    credentials: true,
+  }),
+);
 
 app.get("/api/test_", async (req, res) => {
   try {
-    const sql = `SELECT user.u_id, user.u_name, user.u_surname, user.password, role.role, status.status FROM user
+    const sql = `SELECT user.u_id, user.u_name, user.u_surname, status.status FROM user
     INNER JOIN role ON user.role = role.r_id
     INNER JOIN status ON user.status = status.s_id;`;
     const [products] = await DB.query(sql);
     res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.get("/api/meet", async (req, res) => {
+  try {
+    const sql = `SELECT meeting_room.r_id, meeting_room.r_name, status.status FROM meeting_room
+    INNER JOIN status on meeting_room.status = status.s_id`;
+    const [products] = await DB.query(sql);
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.get("/api/meet/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const sql = `SELECT meeting_room.r_id, meeting_room.r_name, status.status FROM meeting_room
+    INNER JOIN status on meeting_room.status = status.s_id WHERE meeting_room.r_id = ?`;
+    const [products] = await DB.query(sql, [id]);
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.get("/api/status", async (req, res) => {
+  try {
+    const sql = `SELECT * FROM status`;
+    const [products] = await DB.query(sql);
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+app.put("/api/meet/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { r_name, status } = req.body;
+    console.log(req.body);
+    console.log("id:", id, "r_name:", r_name, "status:", status);
+    const sql = `UPDATE meeting_room SET r_id = ?, r_name = ?, status = ? WHERE r_id = ?`;
+    const [result] = await DB.query(sql, [id, r_name, status, id]);
+    res.json({ success: true, message: "Update Success" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -46,24 +98,32 @@ app.post("/login", async (req, res) => {
         message: "Password ไม่ถูกต้อง",
       });
     }
-    if (user.status !== "Ready"){
+    if (user.status !== "Ready") {
       return res.json({
         success: false,
         message: "บัญชีผู้ใช้นี้ถูกระงับการใช้งาน",
       });
     }
 
-    // Login สำเร็จ
+    const userData = {
+      id: user.u_id,
+      name: user.u_name,
+      surname: user.u_surname,
+      role: user.role,
+      status: user.status,
+    };
+
+    res.cookie("user", userData, {
+      httpOnly: true, // JS ฝั่ง Browser อ่านไม่ได้
+      secure: false, // true เมื่อใช้ HTTPS
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 60 * 24, // 1 วัน
+    });
+
     res.json({
       success: true,
       message: "Login Success",
-      user: {
-        id: user.u_id,
-        name: user.u_name,
-        surname: user.u_surname,
-        role: user.role,
-        status: user.status,
-      },
+      user: userData,
     });
   } catch (err) {
     console.error(err);
@@ -73,6 +133,29 @@ app.post("/login", async (req, res) => {
       message: err.message,
     });
   }
+});
+app.get("/profile", (req, res) => {
+  const user = req.cookies.user;
+
+  if (!user) {
+    return res.status(401).json({
+      success: false,
+      message: "Not Login",
+    });
+  }
+
+  res.json({
+    success: true,
+    user,
+  });
+});
+app.post("/logout", (req, res) => {
+  res.clearCookie("user");
+
+  res.json({
+    success: true,
+    message: "Logout Success",
+  });
 });
 
 app.listen(port, "0.0.0.0", () => {
